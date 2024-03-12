c218b7de7517f0249c427c814e25487f4d779767
Gloas <ghislain.loas@gmail.com>
2024-03-12 01:20:23 +0100

    order recieps by alpha last name

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # helloasso_recieps
-Download all the recieps of customers of a sell
+Télécharger la liste des clients de votre boutique au format CSV à l'adresse suivante :  https://admin.helloasso.com/"mon-asso"/boutiques/"ma-boutique"/statistiques
+
+Déposer le fichier ici avec le nom : export-paiements.csv
+
+Inspecter la requête suivante avec votre navigateur :
+https://www.helloasso.com/associations/"mon-asso"/boutiques/"ma-boutique"/paiement-attestation/"oredr_id"
+Puis copier-coller le contenu du paramêtre Cookie de la requête envoyée lorsque vous télécharger manuellement une facture d'un client ici, dans le fichier cookies.txt
+
+lancer la commande
+php download_recieps.php "ma-boutique"
+
+Consulter les pdf dans le dossier recieps
--- download_recieps.php
+++ download_recieps.php
@@ -1,32 +1,43 @@
 <?php
-class Download_Recieps {
+class Download_Recieps
+{
     
-    protected string $_file_path = '/home/ghislo/APE/helloasso/export-paiements.csv';
-    protected string $_cookie_path = '/home/ghislo/APE/helloasso/cookie';
-    protected string $_folder_path = '/home/ghislo/APE/helloasso/downloaded_paiements/';
+o    protected string $_file_path = 'export-paiements.csv';
+    protected string $_cookie_path = 'cookies.txt';
+    protected string $_folder_path = 'recieps/';
+    protected string $_download_url = 'https://www.helloasso.com/associations/association-des-parents-d-eleves-de-valleiry/boutiques/%s/paiement-attestation/%s';
     protected $_context;
 
 
-    public function __construct(string $sell_name) {
-        $csv = file_get_contents($this->_file_path);
-        $csv_as_array = str_getcsv($csv, ';');
-        $csv_as_array = array_filter($csv_as_array, fn($data) => 0 === strpos($data,
-                                                                              sprintf('https://www.helloasso.com/associations/association-des-parents-d-eleves-de-valleiry/boutiques/%s/paiement-attestation/',
-                                                                                      $sell_name)));
+    public function __construct(string $sell_name)
+    {
+        $csv_as_array = array_map('str_getcsv', file($this->_file_path));
+        array_shift($csv_as_array);
+        $csv_as_array = array_map(fn(array $row) => explode(';', reset($row)), $csv_as_array);
+        $unique_csv = [];
+
+        foreach ($csv_as_array as $row)
+            $unique_csv[$row[0]] ??= $row;
+
+        usort($unique_csv, fn($a, $b) => strcasecmp($a[3], $b[3]));
+
         @mkdir($this->_folder_path, 0777, true);
 
         $cookie = file_get_contents($this->_cookie_path);
         $context_params = ['http' => ['method' => 'GET',
                                       'header' => 'Cookie: ' . $cookie]];
         $this->_context = stream_context_create($context_params);
-        array_map(fn($url) => $this->_download($url), $csv_as_array);
+        array_map(fn($row) => $this->_download($row, $sell_name), $unique_csv);
     }
 
 
-    protected function _download(string $url) : void {
-        $file_name = $this->_folder_path . basename($url);
-        if ( !file_exists($file_name))
-            file_put_contents($file_name, file_get_contents($url, false, $this->_context));
+    protected function _download(array $row, string $sell_name): void
+    {
+        $url = sprintf($this->_download_url, $sell_name, $row[0]);
+        $file_name = $this->_folder_path . basename($row[3].'_'.$row[0]) . '.pdf';
+        @unlink($file_name);
+        echo 'Téléchargement de ' . $file_name . "\n";
+        @file_put_contents($file_name, file_get_contents($url, false, $this->_context));
     }
 }
 
