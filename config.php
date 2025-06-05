222fb277ce4cd26bb527634c40f3cfc882cc2724
Gloas <ghislain.loas@gmail.com>
2025-06-05 22:45:53 +0200

    remove tests

--- config.php
+++ config.php
@@ -1,10 +1,4 @@
 <?php
-// Désactiver les warnings de session pour les tests
-if (PHP_SAPI === 'cli') {
-    ini_set('session.sid_length', '32');
-    ini_set('session.sid_bits_per_character', '5');
-}
-
 return [
     'download_url' => 'https://www.helloasso.com/associations/association-des-parents-d-eleves-de-valleiry/boutiques/%s/paiement-attestation/%s',
     'file_path' => 'export-paiements.csv',
